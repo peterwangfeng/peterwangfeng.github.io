460f23e3937f7d90ba4d1b560b3b62c68a6d42b2
wangfeng <1692655013@qq.com>
2021-06-10 18:35:56 +0800

    'test'

---
+++ precache-manifest.c5da05440fb571c0b40cb86f59b7e127.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01ce1440aca69f5eb91d",
+    "url": "css/app.1c17b6db.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "40f81930270cda9fac534d66b296a6e4",
+    "url": "index.html"
+  },
+  {
+    "revision": "01ce1440aca69f5eb91d",
+    "url": "js/app.0e3501c5.js"
+  },
+  {
+    "revision": "7e1bd26480029849fa5b",
+    "url": "js/chunk-vendors.e9f84652.js"
+  },
+  {
+    "revision": "546636ddbc25fef8fb2264f60ecdb29a",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
